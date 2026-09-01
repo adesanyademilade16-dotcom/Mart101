7710e183e36993195ee992bb52313f2dcff6aa41
Gremlin <mikejoel895@gmail.com>
2026-09-01 03:38:47 +0100

    Update fetch event to use no-store cache

--- public/sw.js
+++ public/sw.js
@@ -2,5 +2,5 @@ self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", () => self.clients.claim());
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(fetch(event.request, { cache: "no-store" }));
 });
